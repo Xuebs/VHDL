a2e282d12e74ceed8578f6be6dbc4d2226f4eed7
Xuebs <tianxuebin@hotmail.com>
2018-03-29 14:45:20 -0400

    lab 4

--- Code/lab4.vhd
+++ Code/lab4.vhd
@@ -3,16 +3,28 @@ use IEEE.std_logic_1164.all;
 use IEEE.std_logic_unsigned.all;
 
 entity registers_min_max is
-port ( 	din 	: 	in std_logic_vector(3 downto 0);
-	reset 	: 	in std_logic;
-	clk	:	in std_logic;
-	sel	:	in std_logic_vector(1 downto 0);
-	max_out	:	out std_logic_vector(3 downto 0);
-	min_out	:	out std_logic_vector(3 downto 0);
-	reg_out	:	out std_logic_vector(3 downto 0);)
+	port ( 	din 	: 	in std_logic_vector(3 downto 0);
+			reset 	: 	in std_logic;
+			clk	:	in std_logic;
+			sel	:	in std_logic_vector(1 downto 0);
+			max_out	:	out std_logic_vector(3 downto 0);
+			min_out	:	out std_logic_vector(3 downto 0);
+			reg_out	:	out std_logic_vector(3 downto 0);)
 end registers_min_max;
 
 architecture arch of registers_min_max is
 
+	variable reg1: std_logic_vector(3 left downto 0);
+	variable reg2: std_logic_vector(3 left downto 0);
+	variable reg3: std_logic_vector(3 left downto 0);
+	variable reg4: std_logic_vector(3 left downto 0);
+
+	updateReg: process (clk)
+	begin
+	
+	end process;
+
 begin
+
+end arch;
 	
