6bcc1f30ceff27b89630ea38e400b841e066190b
Xuebs <tianxuebin@hotmail.com>
2018-03-29 17:24:08 -0400

    ieee

--- Code/lab4.vhd
+++ Code/lab4.vhd
@@ -1,6 +1,6 @@
-library IEEE;
-use IEEE.std_logic_1164.all;
-use IEEE.std_logic_unsigned.all;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_unsigned.all;
 
 entity registers_min_max is
 	port ( 	din 	: 	in std_logic_vector(3 downto 0);
