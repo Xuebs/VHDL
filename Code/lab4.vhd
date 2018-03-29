04b1f4b54ebfd517e73a72e94761ab703cf97849
Xuebs <tianxuebin@hotmail.com>
2018-03-29 17:21:17 -0400

    quick fix

--- Code/lab4.vhd
+++ Code/lab4.vhd
@@ -4,12 +4,12 @@ use IEEE.std_logic_unsigned.all;
 
 entity registers_min_max is
 	port ( 	din 	: 	in std_logic_vector(3 downto 0);
+			sel	:	in std_logic_vector(1 downto 0);
 			reset 	: 	in std_logic;
 			clk	:	in std_logic;
-			sel	:	in std_logic_vector(1 downto 0);
 			max_out	:	out std_logic_vector(3 downto 0);
 			min_out	:	out std_logic_vector(3 downto 0);
-			reg_out	:	out std_logic_vector(3 downto 0);)
+			reg_out	:	out std_logic_vector(3 downto 0));
 end registers_min_max;
 
 architecture arch of registers_min_max is
