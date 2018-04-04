304f67307226d914c0f55c326486a22a93751560
Xuebs <tianxuebin@hotmail.com>
2018-04-04 02:42:07 -0400

    combined clock and reset

--- Code/registers_min_max.vhd
+++ Code/registers_min_max.vhd
@@ -13,13 +13,14 @@ entity registers_min_max is
 end registers_min_max;
 
 architecture arch of registers_min_max is
-	signal register1 : std_logic_vector(3 downto 0);
-	signal register2 : std_logic_vector(3 downto 0);
-	signal register3 : std_logic_vector(3 downto 0);
-	signal register4 : std_logic_vector(3 downto 0);
+
+signal register1 : std_logic_vector(3 downto 0);
+signal register2 : std_logic_vector(3 downto 0);
+signal register3 : std_logic_vector(3 downto 0);
+signal register4 : std_logic_vector(3 downto 0);
 
 begin
-	updateReg: process (clk) is
+	updateReg: process (clk, reset) is
 		variable reg1 : std_logic_vector(3 downto 0);
 		variable reg2 : std_logic_vector(3 downto 0);
 		variable reg3 : std_logic_vector(3 downto 0);
@@ -45,18 +46,25 @@ begin
 			register1 (3 downto 0) <= reg1 (3 downto 0);
 		end if;
 
-	end process updateReg;
-
-	resetReg: process (reset) is
-	
-	begin
 		if (reset'event and reset = '1') then
 			register1 (3 downto 0)<= "1000";
 			register2 (3 downto 0)<= "1000";
 			register3 (3 downto 0)<= "1000";
 			register4 (3 downto 0)<= "1000";
 		end if;
-	end process resetReg;
+
+	end process updateReg;
+
+	-- resetReg: process (reset) is
+	
+	-- begin
+	-- 	if (reset'event and reset = '1') then
+	-- 		register1 (3 downto 0)<= "1000";
+	-- 		register2 (3 downto 0)<= "1000";
+	-- 		register3 (3 downto 0)<= "1000";
+	-- 		register4 (3 downto 0)<= "1000";
+	-- 	end if;
+	-- end process resetReg;
 
 	selReg: process (sel) is
 	begin
